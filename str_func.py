85b1e21114a5a76df16e0815f58199e35d923e9d
Maxim Shurygin <maximys2142@mail.ru>
2023-05-25 12:30:47 +0800

    added new func and docstrings

--- str_func.py
+++ str_func.py
@@ -1,3 +1,8 @@
 def capitalize_string(string):
+    '''функция возвращает строку большими буквами'''
     return string.upper()
 
+
+def capitalize_first_letters(string):
+    '''функция возвращает строку с первыми большими буквами слов'''
+    return string.capitalize()
